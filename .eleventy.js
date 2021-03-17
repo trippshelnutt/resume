e908f3f8fab60497cb32559e8d261adf02863a6e
Tripp Shelnutt <trippshelnutt@outlook.com>
2021-03-16 20:51:14 -0400

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget(".tmp/css/custom.css");
     eleventyConfig.addPassthroughCopy({".tmp/css/custom.css": "css/custom.css"});
 
+    eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy("resume-trippshelnutt.pdf");
 }
